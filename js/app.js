1bb64741986f109c503f6181dcd68b0870dc7712
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-08-18 10:22:13 -0600

    made changes to code for inputs that are NaN, below 0, or above 100. Explained code with comments for future reference.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	var input = 100
 	var counter = 0
+	// prevent side-effects by equalling variable to function
 	var randomNumber = generateRandomNumber()
 
 	/*--- Display information modal box ---*/
@@ -15,6 +16,7 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	/*--- New game button ---*/
 
   	$(".new").click(function clearAll(event){
   		event.preventDefault();
@@ -22,37 +24,49 @@ $(document).ready(function(){
   		$("#guessList").empty()
   		counter = 0
 	    document.getElementById("count").innerHTML = counter
+	    // generates new random number, dont use var infront because this re-declares variable
 	    randomNumber = generateRandomNumber()
 	 
 	});
 	
 	/*---  to get the user's input ---*/
 
+	// to get the user's entered input
 	$("#userGuess").keyup(function () {
 	    input = document.getElementById("userGuess").value
 	});
 	
-
+	// to display an alert message if input was below 0, above 100 or it was a string 
 	$("#guessForm").submit(function (){
 		var userInput = Number(input)
-		if(userInput < 0 || userInput > 100){
+		if(userInput < 0 || userInput > 100 || isNaN(userInput)){
   			alert('Please enter a number between zero and 100');
   			return true;
   		}
     });
-
+	// generates a random number
 	function generateRandomNumber(){
 		 var n = Math.ceil(Math.random() * 100);
 		 return n
 	}
-	console.log("YISS")
+	
 		
-    /*---  to submit a guess ---*/
+    /*---  to submit a guess and give feedback to the user ---*/
 
     $("#guessForm").submit(function (event){
     	event.preventDefault();
+    	// counter counts the number of times a user submited an inpu
     	counter++
+    	// to make input a number
     	var userInput = Number(input)
+    	
+    	// conditions to give feedback to user
+
+    	// this first if statement will empty input box if user entered a string, a number below 0, number above 100
+    	if(userInput < 0 || userInput > 100 || isNaN(userInput)){
+    		document.getElementById("userGuess").value = ""
+  			return true;
+  		}
     	var inputVsRandom = randomNumber - userInput;
     	
 		if (randomNumber === userInput){
@@ -71,8 +85,16 @@ $(document).ready(function(){
 			document.getElementById("feedback").innerHTML = "Ice cold"
 		}
 
+		// appends input to guess list for user to keep record of guesses
+		
 		$("#guessList").append("<li>" + userInput + "</li>");
+		
+		// tells user the number of guesses
+		
 		document.getElementById("count").innerHTML = counter
+		
+		// empties input box after the user submits an input
+    	
     	document.getElementById("userGuess").value = ""
    
   	});
